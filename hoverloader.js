c3f980bb83560621daaceaf3c713b2eda7c8f76d
midzer <midzer@gmail.com>
2019-01-01 10:46:25 +0100

    insert another possibility

--- hoverloader.js
+++ hoverloader.js
@@ -11,3 +11,19 @@ for (var i = 0; i < elements.length; i++){
         document.head.removeChild(res);
     });
 }
+
+/*var res = document.createElement("link");
+res.rel = "prefetch";
+var elements = Array.from(document.querySelectorAll("a")).filter(function(item) {
+    return !((/\.(gif|jpg|jpeg|tiff|png|pdf)$/i).test(item.target.href))
+});
+for (var i = 0, j = elements.length; i < j; i++){
+    elements[i].addEventListener('mouseenter',function(e){
+        res.href = e.target.href;
+        document.head.appendChild(res);
+    });
+    elements[i].addEventListener('mouseleave',function(){
+        document.head.removeChild(res);
+    });
+}
+*/
